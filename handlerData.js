39289f2935e30ee2bb0e55dd615b27c6e68bcbe4
VitaliiMysko <61671490+VitaliiMysko@users.noreply.github.com>
2024-10-31 20:32:10 +0200

    Change button logic and improve performance (#4)
    * done
    
    * refactoring
    
    ---------
    
    Co-authored-by: Vitalii Mysko <ext-vitalii.mysko@flowbird.group>

---
+++ handlerData.js
@@ -0,0 +1,193 @@
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "getData") {
+    const data = getData();
+    sendResponse({ data });
+  }
+  return true; // Потрібно для асинхронного sendResponse
+});
+
+function getData() {
+  let data = [];
+  let generalData = [];
+  let companiesAndJobPosition = [];
+
+  try {
+    generalData = getGeneralData();
+    companiesAndJobPosition = getCompaniesAndJobPosition();
+  } catch (error) {
+    console.log("Probably, something bad happened with getting data");
+    console.log(error.message);
+  }
+
+  data.push({ category: "generalData", value: generalData });
+  data.push({ category: "comany&jobPosition", value: companiesAndJobPosition });
+
+  return data;
+
+  //>>>>>>>>>>>>>>>>>>GET GENERAL_DATA<<<<<<<<<<<<<<<<<<
+  function getGeneralData() {
+    let generalData = [];
+    const fullName = getFullName();
+    generalData.push({ inputId: "firstName", value: getFirstName(fullName) });
+    generalData.push({ inputId: "secondName", value: getSecondName(fullName) });
+    generalData.push({ inputId: "jobPosition", value: getJobPosition() });
+    generalData.push({ inputId: "link", value: getLinkedinLink() });
+    generalData.push({ inputId: "email", value: "" });
+    generalData.push({ inputId: "companyName", value: "" });
+
+    return generalData;
+  }
+
+  function getFullName() {
+    const element = document.querySelector(
+      'h1[data-x--lead--name][data-anonymize="person-name"]'
+    );
+    return element.textContent.trim();
+  }
+
+  function getFirstName(fullName) {
+    let [firstName] = fullName.includes(" ") ? fullName.split(" ") : "";
+    return firstName.trim();
+  }
+
+  function getSecondName(fullName) {
+    let [, ...remainingWords] = fullName.split(" ");
+    let secondName = remainingWords.length > 0 ? remainingWords.join(" ") : "";
+    return secondName.trim();
+  }
+
+  function getJobPosition() {
+    return "";
+  }
+
+  function getLinkedinLink() {
+    let xpath = "//a[contains(@href, 'https://www.linkedin.com/in/')]"; // XPath вираз
+
+    let result = document.evaluate(
+      xpath,
+      document,
+      null,
+      XPathResult.FIRST_ORDERED_NODE_TYPE,
+      null
+    );
+
+    let element = result.singleNodeValue;
+    let link = element ? element.href : "";
+    return link.trim();
+  }
+
+  //>>>>>>>>>>>>>>>>>>GET GENERAL_DATA<<<<<<<<<<<<<<<<<<
+  function getCompaniesAndJobPosition() {
+    let comanyjobPositionData = [];
+
+    const companyComponents = document.querySelectorAll(
+      "._experience-entry_1irc72"
+    );
+
+    let id = 0;
+
+    for (const companyComponent of companyComponents) {
+      let name = "";
+      let link = "";
+      let jobPosition = "";
+
+      const companyDataElement = companyComponent.children[0].children[1];
+
+      link = GetCompanyLink(companyDataElement);
+      name = GetCompanyName(companyDataElement);
+
+      const jobPositionElement = GetJobPositionElement(companyDataElement);
+
+      if (jobPositionElement) {
+        jobPosition = jobPositionElement.textContent.trim();
+
+        const actualPositionElement = companyDataElement.children[2];
+
+        if (IsActualJobPosition(actualPositionElement)) {
+          if (name != "" && jobPosition != "") {
+            comanyjobPositionData.push({
+              id: ++id,
+              name: name,
+              jobPosition: jobPosition,
+              link: link,
+            });
+          }
+        } else {
+          break;
+        }
+      } else {
+        const multiPositionCompanyComponent =
+          companyComponent.querySelector("ul");
+
+        if (multiPositionCompanyComponent) {
+          const positionComponents =
+            multiPositionCompanyComponent.querySelectorAll("li");
+
+          for (const positionComponent of positionComponents) {
+            const jobPositionElement = GetJobPositionElement(positionComponent);
+
+            if (jobPositionElement) {
+              jobPosition = jobPositionElement.textContent.trim();
+            }
+
+            const actualPositionElement =
+              positionComponent.children[1].children[1];
+
+            if (IsActualJobPosition(actualPositionElement)) {
+              if (name != "" && jobPosition != "") {
+                comanyjobPositionData.push({
+                  id: ++id,
+                  name: name,
+                  jobPosition: jobPosition,
+                  link: link,
+                });
+              }
+            } else {
+              break;
+            }
+          }
+        }
+      }
+    }
+    return comanyjobPositionData;
+  }
+
+  function GetCompanyLink(element) {
+    let link = "";
+
+    const linkElement = element.querySelector("a");
+
+    if (linkElement) {
+      link = linkElement.href;
+      link = link.includes("sales/company") ? link : "";
+    }
+    return link;
+  }
+
+  function GetCompanyName(element) {
+    let name = "";
+    let companyElement = element.querySelector(
+      '[data-anonymize="company-name"]'
+    );
+
+    if (companyElement) {
+      name = companyElement.textContent.trim();
+    }
+    return name;
+  }
+
+  function GetJobPositionElement(element) {
+    return element.querySelector('[data-anonymize="job-title"]');
+  }
+
+  function IsActualJobPosition(element) {
+    if (element) {
+      const periodElement = element.querySelector("span");
+
+      if (periodElement) {
+        const period = periodElement.textContent.trim();
+        return period.includes("Present");
+      }
+    }
+  }
+}
