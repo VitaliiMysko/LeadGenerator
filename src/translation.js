c17d6ac830e7a96d4a18b1b6e66ea77390768c75
Vitalii Mysko <ext-vitalii.mysko@flowbird.group>
2024-11-07 21:07:25 +0200

    done

---
+++ src/translation.js
@@ -0,0 +1,38 @@
+document.getElementById("translateBtn").addEventListener("click", () => {
+  chrome.runtime.sendMessage({ action: "getAuthToken" }, (response) => {
+    if (response.success) {
+      translateText(response.token);
+    } else {
+      console.error("Error authorization:", response.error);
+    }
+  });
+});
+
+function translateText(token) {
+  const url = "https://translation.googleapis.com/language/translate/v2";
+  const jobPositionElement = document.getElementById("jobPositionInput");
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
+    },
+    body: JSON.stringify({
+      q: jobPositionElement.value,
+      target: "en",
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data && data.data && data.data.translations) {
+        const translations = data.data.translations;
+        jobPositionElement.value = translations[0].translatedText;
+      } else {
+        console.error("Error translating:", data);
+      }
+    })
+    .catch((error) =>
+      console.error("Error query to translation service:", error)
+    );
+}
