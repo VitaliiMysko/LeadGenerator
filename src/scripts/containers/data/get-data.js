3614951e5fee3c34c021c6b223df299fee7e112a
Vitalii Mysko <ext-vitalii.mysko@flowbird.group>
2024-12-02 18:20:03 +0200

    added_get_data_file

---
+++ src/scripts/containers/data/get-data.js
@@ -0,0 +1,44 @@
+import { getBtnElement } from "../../helper/dom-helper.js";
+import { createRadioCompaniesList } from "../companies/companies-list.js";
+
+getBtnElement.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: tabs[0].id },
+        files: ["src/content-scripts/extract-data.js"],
+      },
+      () => {
+        chrome.tabs.sendMessage(
+          tabs[0].id,
+          { action: "getData" },
+          (results) => {
+            if (results) {
+              const data = results.data;
+              data.forEach((element) => {
+                if (element.category === "generalData") {
+                  populateGeneralData(element.value);
+                }
+                if (element.category === "comany&jobPosition") {
+                  createRadioCompaniesList(element.value);
+                }
+              });
+            }
+          }
+        );
+      }
+    );
+  });
+});
+
+function populateGeneralData(items) {
+  items.forEach((item) => {
+    if (item.inputId == "first-name" || item.inputId == "second-name") {
+      document.querySelector(`#${item.inputId}`).value = transliterate(
+        item.value
+      );
+    } else {
+      document.querySelector(`#${item.inputId}`).value = item.value;
+    }
+  });
+}
