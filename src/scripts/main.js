8f5078de9f230efbadea934be8bd334794b3f86b
Vitalii Mysko <ext-vitalii.mysko@flowbird.group>
2024-12-05 22:19:54 +0200

    done

---
+++ src/scripts/main.js
@@ -0,0 +1,13 @@
+import {
+    appVerionElement,
+  } from "./helper/dom-helper.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const manifest = chrome.runtime.getManifest();
+
+  if (appVerionElement) {
+    const environment = manifest.environment;
+    const showEnvironment = environment ? ` (${environment})` : environment;
+    appVerionElement.textContent = `Version: ${manifest.version}${showEnvironment}`;
+  }
+});
