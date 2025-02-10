472aace6eb4f31445268ab317aef8142ab54766e
VitaliiMysko <61671490+VitaliiMysko@users.noreply.github.com>
2025-02-10 18:59:23 +0200

    done (#39)
    Co-authored-by: Vitalii Mysko <ext-vitalii.mysko@flowbird.group>

--- src/content-scripts/experience-data.js
+++ src/content-scripts/experience-data.js
@@ -102,7 +102,10 @@ if (!window.leadGenerator.experienceDataInit) {
 
       if (companyElement) {
         const regex = removeCompanyStatusRegex();
-        name = companyElement.textContent.trim().replace(regex, "");
+        name = companyElement.textContent
+          .trim()
+          .replace(/[^\p{L}\p{N}\s-]/gu, "")
+          .replace(regex, "");
       }
       return name;
     }
