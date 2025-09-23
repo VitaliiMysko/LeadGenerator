da384a89093f188711abd139269f0c5610a26543
SidelnykOleksii <40484752+SidelnykOleksii@users.noreply.github.com>
2025-09-23 14:18:26 +0300

    LG-115 Fix transliteration (#102)
    * added Unicode character range, fixed tiny typo
    
    * removed extra lines
    
    * format document
    
    ---------
    
    Co-authored-by: oleksii_laptop <formovka4@ukr.net>

--- src/content-scripts/sales-navigator-pages/lead/lead.js
+++ src/content-scripts/sales-navigator-pages/lead/lead.js
@@ -25,20 +25,20 @@ if (!window.leadGenerator.personalDataInit) {
     };
 
     function getFullName() {
-      const fullNamelement = getFullNameElement();
+      const fullNameElement = getFullNameElement();
 
-      if (!fullNamelement) return "";
+      if (!fullNameElement) return "";
 
-      return handleFullName(fullNamelement.textContent);
+      return handleFullName(fullNameElement.textContent);
     }
 
     function handleFullName(str) {
-      str = str.replace(/^[^a-zA-Z]+/, "");
+      str = str.replace(/^[^a-zA-Z\u00C0-\u017F]+/, "");
       // Removes the prefix dr/Dr/prof/Prof before the full name
       str = str
         .trim()
         .replace(
-          /^(prof\.?\s+)?(prof\.?|prof\,?|dr\.?|dr\,?|dr\.-ing\.?)\s+(?=[A-Z-İÜÖÇĞŞ])/i,
+          /^(prof\.?\s+)?(prof\.?|prof\,?|dr\.?|dr\,?|dr\.-ing\.?)\s+(?=[A-Z\u00C0-\u017F])/i,
           ""
         );
       const [textBeforeComma] = str.split(",");
