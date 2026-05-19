45e866d266c8ebb9efd2b25447ead5c502882d51
Vitalii Mysko <ext-vitalii.mysko@flowbird.group>
2026-05-19 20:39:41 +0300

    done

--- src/scripts/containers/data/open-company-linkedin.js
+++ src/scripts/containers/data/open-company-linkedin.js
@@ -3,6 +3,6 @@ import { openCompanyLinkedinBtnElement } from "../../helper/dom-helper.js";
 openCompanyLinkedinBtnElement.addEventListener("click", () => {
   const href = openCompanyLinkedinBtnElement.dataset.href;
   if (href) {
-    chrome.tabs.create({ url: href });
+    chrome.tabs.create({ url: href.replace("/sales/", "/"), active: false });
   }
 });
